bdd1c21f6c6d2dbee14662b64320f4e72d323670
Jon Berry <jonpber@gmail.com>
2017-05-05 22:17:57 -0700

    first setting up on site

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,7 +4,6 @@ var ties = 0;
 
 var winText = document.getElementById("wins");
 var lossText = document.getElementById("losses");
-var tieText = document.getElementById("ties");
 var choiceText = document.getElementById("compPlayerChoice");
 
 var compHand = ["r","p","s"];
@@ -12,62 +11,59 @@ var compHand = ["r","p","s"];
 var pHand = "";
 var CompHandPick = "";
 
-document.onkeypress = function(event){
-	if (event.key.toLowerCase() === "r"){
-		pHand = "r";
-		if (CompPick() === "r"){
-			ties += 1;
-		}
-
-		else if (CompPick() === "s"){
-			wins += 1;
-		}
-
-		else {
-			losses += 1;
-		}
-
-		updateScore();
+function buttonR () {
+	pHand = "r";
+	if (CompPick() === "r"){
+		ties += 1;
 	}
 
-
-
-	else if (event.key.toLowerCase() === "p"){
-		pHand = "p";
-		if (CompPick() === "p"){
-			ties += 1;
-		}
-
-		else if (CompPick() === "r"){
-			wins += 1;
-		}
-
-		else {
-			losses += 1;
-		}
-
-		updateScore();
-
+	else if (CompPick() === "s"){
+		wins += 1;
 	}
 
-	else if (event.key.toLowerCase() === "s"){
-		pHand = "s";
-		if (CompPick() === "s"){
-			ties += 1;
-		}
-
-		else if (CompPick() === "p"){
-			wins += 1;
-		}
-
-		else {
-			losses += 1;
-		}
-
-		updateScore();
-
+	else {
+		losses += 1;
 	}
+
+	updateScore();
 }
+
+
+function buttonP () {
+	pHand = "p";
+	if (CompPick() === "p"){
+		ties += 1;
+	}
+
+	else if (CompPick() === "r"){
+		wins += 1;
+	}
+
+	else {
+		losses += 1;
+	}
+
+	updateScore();
+
+}
+
+function buttonS () {
+	pHand = "s";
+	if (CompPick() === "s"){
+		ties += 1;
+	}
+
+	else if (CompPick() === "p"){
+		wins += 1;
+	}
+
+	else {
+		losses += 1;
+	}
+
+	updateScore();
+}
+
 
 function CompPick () {
 	compHandPick = compHand[Math.floor(Math.random() * 3)];
@@ -77,7 +73,6 @@ function CompPick () {
 function updateScore(){
 	winText.textContent = "Wins: " + wins;
 	lossText.textContent = "Loses: " + losses;
-	tieText.textContent = "Ties: " + ties;
 	choiceText.textContent = "Player Chose: " + pHand + " -- Comp chose: " + compHandPick;
 }
 
