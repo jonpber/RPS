4efeadf7ea3deb7434a32c36f8537942f67de859
Jon Berry <jonpber@gmail.com>
2017-05-07 10:07:39 -0700

    cleaned up code some

---
+++ assets/javascript/logic1.js
@@ -0,0 +1,407 @@
+var rps = {
+	wins: 0,
+	losses: 0,
+	ties: 0,
+
+	chipText: document.getElementById("chips"),
+	betText: document.getElementById("bet"),
+	potText: document.getElementById("pot"),
+	betNum: 0,
+	chips: 50,
+	pot: 0,
+
+	stateText: document.getElementById("stateText"),
+
+	pImg: document.getElementById("pHandImg"),
+	compImg: document.getElementById("cHandImg"),
+
+	splitRow: document.getElementById("splitRow"),
+	onlyHandRow: document.getElementById("onlyHand"),
+	twoHandRow: document.getElementById("twoHands"),
+	pHandABut: document.getElementsByClassName("twoHandAButton"),
+	pHandBBut: document.getElementsByClassName("twoHandBButton"),
+	betRow: document.getElementById("betRow"),
+
+	compHand: ["r","p","s"],
+
+	pHand: "",
+	pHandA: "",
+	pHandB: "",
+	compHandPick: "",
+	compHandA: "",
+	compHandB: "",
+
+	pHAImg: document.getElementById("p2HandA"),
+	pHBImg: document.getElementById("p2HandB"),
+	cHAImg: document.getElementById("c2HandA"),
+	cHBImg: document.getElementById("c2HandB"),
+	pWon: false,
+	tie: false,
+	splitBattle: false,
+
+	lastBet: 0,
+
+	oneHandChoices: document.getElementsByClassName("oneHandButton"),
+
+	hyperbole: [" was thoroughly destroyed by ", " faced bitter defeat by ", " lost to "],
+
+	chips: 50,
+
+	stand: function(){
+		//turns off the tie bar and returns to default
+		this.splitRow.style.display = "none";
+		this.onlyHandRow.style.display = "block";
+		this.betRow.style.display = "block";
+
+		this.stateText.textContent = "I guess you're not a risk-taker."
+
+		//Resets bet, turns off tie, takes back share of pot, and resets it to 0.
+		this.betNum = 0;
+		this.tie = false;
+		this.pot /= 2;
+		this.getPot();
+		this.potTo0(this.potText);
+	},
+
+	split: function(){
+		this.stateText.textContent = "Pick two hands!";
+		this.splitRow.style.display = "none";
+		this.twoHandRow.style.display = "block";
+		this.pImg.style.display = "none";
+		this.compImg.style.display = "none";
+
+	},
+
+	compPick: function () {
+		if (!this.splitBattle){
+			this.compHandPick = this.compHand[Math.floor(Math.random() * 3)];
+			this.compImg.src = "assets/images/cHand" + this.compHandPick + ".png";
+		}
+
+		else {
+			this.compHandA = this.compHand[Math.floor(Math.random() * 3)];
+			this.cHAImg.src = "assets/images/cHandA" + this.compHandPick + ".png";
+
+			this.compHandB = this.compHand[Math.floor(Math.random() * 3)];
+			this.cHAImg.src = "assets/images/cHandB" + this.compHandPick + ".png";
+		}
+
+
+	},
+
+	handsCheck(x, y){
+		if (x === "r"){
+			if (y === "s"){
+				this.pWon = true;
+				this.tie = false;
+				return "p";
+			}
+
+			else if (y === "p"){
+				this.pWon = false;
+				this.tie = false;
+				return "c";
+			}
+
+			else {
+				this.pWon = false;
+				this.tie = true;
+				return "t";
+			}
+		}
+
+		else if (x === "s"){
+			if (y === "p"){
+				this.pWon = true;
+				this.tie = false;
+				return "p";
+			}
+
+			else if (y === "r"){
+				this.pWon = false;
+				this.tie = false;
+				return "c";
+			}
+
+			else {
+				this.pWon = false;
+				this.tie = true;
+				return "t";
+			}
+		}
+
+		else {
+			if (y === "r"){
+				this.pWon = true;
+				this.tie = false;
+				return "p";
+			}
+
+			else if (y === "s"){
+				this.pWon = false;
+				this.tie = false;
+				return "c";
+			}
+
+			else {
+				this.pWon = false;
+				this.tie = true;
+				return "t";
+			}
+		}
+	},
+
+	gamelogic: function(){
+		if(!this.split){
+			var winner = this.handsCheck(this.pHand, this.compHand);
+			this.roundEnd(winner);
+		}
+	},
+
+	roundStart: function(){
+		this.bet();
+		this.compPick();
+		this.roundEnd(this.handsCheck(this.pHand, this.compHandPick));
+
+	},
+
+	roundEnd: function (x){
+		this.pImg.style.display = "inline";
+		this.compImg.style.display = "inline";
+
+		//player wins round
+		if (x === "p") {
+			this.stateText.textContent = "Computer " +  this.hyperbole[Math.floor(Math.random() * 3)] + "Player.";
+			
+			//Player gets pot, resets pot, and changes pot text.
+			this.getPot();
+			this.pot = 0;
+			this.betNum = 0;
+			this.potTo0(this.potText);
+
+		}
+
+		//comp wins round
+		else if (x === "c"){
+			this.stateText.textContent = "Player " + this.hyperbole[Math.floor(Math.random() * 3)] + "Computer.";
+			
+			//Player loses pot, resets pot, and changes pot text.
+			this.pot = 0;
+			this.betNum = 0;
+			this.getPot();
+			this.potTo0(this.potText);
+		}
+
+		//round ends in tie
+		else {
+			this.stateText.textContent = "A tie!!! Care to split and double your bet?"
+			
+			//turns on the split bar option
+			this.splitRow.style.display = "block";
+			this.onlyHandRow.style.display = "none";
+			this.betRow.style.display = "none";
+		}
+
+		//Bet Number either resets to 0 or stays consistent
+		this.betText.textContent = this.betNum;
+		this.chipUpdate();
+	},
+
+
+	buttonR: function () {
+		this.pHand = "r";
+		this.pImg.src = "assets/images/pHandr.png";
+		this.roundStart();
+
+	},
+
+	buttonP: function () {
+		this.bet();
+		this.compPick();
+		this.pImg.src = "assets/images/pHandp.png";
+		this.pHand = "p";
+		this.handsCheck(this.pHand, this.compHandPick);
+	},
+
+	buttonS: function () {
+		this.bet();
+		this.compPick();
+		this.pHand = "s";
+		this.pImg.src = "assets/images/pHands.png";
+		this.handsCheck(this.pHand, this.compHandPick);	
+	},
+
+	buttonAR: function(){
+		this.pHAImg.src = "assets/images/p2HandAr.png";
+		this.pHAImg.style.display = "block";
+		this.buttonDisabled(this.pHandABut, 3, true);
+		this.pHandA = "r";
+	},
+
+	buttonAP: function(){
+		this.pHAImg.src = "assets/images/p2HandAp.png";
+		this.pHAImg.style.display = "block";
+		this.buttonDisabled(this.pHandABut, 3, true);
+		this.pHandA = "p";
+	},
+
+	buttonAS: function(){
+		this.pHAImg.src = "assets/images/p2HandAs.png";
+		this.pHAImg.style.display = "block";
+		this.buttonDisabled(this.pHandABut, 3, true);
+		this.pHandA = "s";
+	},
+
+	buttonBR: function(){
+		this.pHBImg.src = "assets/images/p2HandBr.png";
+		this.pHBImg.style.display = "block";
+		this.buttonDisabled(this.pHandBBut, 3, true);
+		this.pHandB = "r";
+	},
+
+	buttonBP: function(){
+		this.pHBImg.src = "assets/images/p2HandBp.png";
+		this.pHBImg.style.display = "block";
+		this.buttonDisabled(this.pHandBBut, 3, true);
+		this.pHandB = "p";
+	},
+
+	buttonBS: function(){
+		this.pHBImg.src = "assets/images/p2HandBs.png";
+		this.pHBImg.style.display = "block";
+		this.buttonDisabled(this.pHandBBut, 3, true);
+		this.pHandB = "s";
+	},
+
+	//should remove your chips as you bet
+	takeOutChips: function(){
+		if(this.lastBet != 0){
+			this.chips -= this.lastBet;
+			this.chipUpdate();
+		}
+		else {
+			this.chips -= this.betNum;
+			this.chipUpdate();
+		}
+	},
+
+	cashout: function(){
+		if (!this.tie && this.pWon){
+			this.chips += this.pot;
+			this.pot = 0;
+		}
+
+		else if (!this.tie && !this.pWon){
+			this.pot = 0;
+		}
+
+	},
+
+	//Takes your bet and matches it by the comp to generate the pot.
+	bet: function(){
+		this.pot = this.betNum * 2;
+		this.potText.textContent = this.pot;
+		this.lastBet = 0;
+	},
+
+	//updates the text content of the chips as well as running the betchecker
+	chipUpdate: function(){
+		this.betText.textContent = this.betNum;
+		this.chipText.textContent = this.chips;
+		this.betCheck();
+	},
+
+	//This function ensures there is a bet before you are allowed to play.
+	betCheck: function(){
+		//if bet is more than 0
+		if (this.betNum > 0){
+			this.buttonDisabled(this.oneHandChoices, 3, false);
+			
+		}
+		else {
+			this.buttonDisabled(this.oneHandChoices, 3, true);
+		}
+	},
+
+	//these buttons add or subtract your bet
+	betMinus10: function() {
+		if (this.betNum - 10 >= 0){
+			this.betNum -= 10;
+			this.lastBet = -10;
+			this.takeOutChips();
+			this.betText.textContent = this.betNum;
+		}
+
+		else {
+			this.betNum = 0;
+			this.betText.textContent = this.betNum;
+		}
+
+		this.betCheck();
+	},
+
+	betMinus1: function() {
+		if (this.betNum - 1 >= 0){
+			this.betNum -= 1;
+			this.lastBet = -1;
+			this.takeOutChips();
+			this.betText.textContent = this.betNum;
+		}
+		this.betCheck();
+
+	},
+
+	betPlus1: function() {
+		if (this.chips - 1 >= 0){
+			this.betNum += 1;
+			this.lastBet = 1;
+			this.takeOutChips();			
+			this.betText.textContent = this.betNum;
+		}
+
+		this.betCheck();
+	},
+
+	betPlus10: function() {
+		if (this.chips - 10 >= 0){
+			this.betNum += 10;
+			this.lastBet = 10;
+			this.takeOutChips();			
+			this.betText.textContent = this.betNum;
+		}
+
+		this.betCheck();
+	},
+
+	//disables/enables buttons
+	buttonDisabled: function (arr, num, truth){
+		for (var i = 0; i < num; i++){
+			arr[i].disabled = truth;
+		}
+	},
+
+	//puts a small buffer between win/loss/stand and pot resuming to 0
+	potTo0: function(x){
+		setTimeout(function () {
+	        this.pot = 0;
+	        x.textContent = this.pot;
+	    	}, 1500);
+	},
+
+	getPot: function(){
+		this.chips += this.pot;
+		this.pot = 0;
+		this.lastBet = 0;
+		this.chipUpdate();
+	},
+
+}
+
+
+
+
+
+
+
+
+
